64b52e8fd4a9af3700850633e0ddacacb897e6a3
kojikawazu <nanairo7.daydream21@gmail.com>
2025-01-25 17:25:19 +0900

    APIデバッグ

--- src/pages/api/gcs/gcs.ts
+++ src/pages/api/gcs/gcs.ts
@@ -1,5 +1,6 @@
 import { Hono } from 'hono';
 import { Storage } from '@google-cloud/storage';
+import fs from 'fs';
 
 // GCSのインスタンスを作成
 const storage = new Storage();
@@ -31,6 +32,11 @@ gcsRouter.get('/data', async (c) => {
     try {
         const bucketName = process.env.GCS_PRIVATE_BUCKET_NAME;
         const fileName = process.env.GCS_JSON_PATH;
+
+        const keyPath = process.env.GOOGLE_APPLICATION_CREDENTIALS || '/app/secrets/gcs/default.json';
+        console.log('Service Account Path:', keyPath);
+        const content = fs.readFileSync(keyPath, 'utf8');
+        console.log('Service Account Content:', content);
 
         if (!bucketName || !fileName) {
             return c.json({ error: 'Bucket name or file name is not set' }, 400);
