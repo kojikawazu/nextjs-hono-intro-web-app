5c1d446c9b99421f9d80f595c676f7d56c3ecc55
kojikawazu <nanairo7.daydream21@gmail.com>
2025-01-25 20:03:18 +0900

    画像対策デバッグ

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    unoptimized: true,
+    domains: ['storage.googleapis.com', 'images.unsplash.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        pathname: '/intro_k_pub_bucket/**',
+      },
+    ],
   },
   reactStrictMode: true,
 };
